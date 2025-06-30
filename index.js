f2e34371dcd07834b9ee3a07a42208704f1e6c96
Sebastian Alpizar <sebastianalpizar26@gmail.com>
2025-06-29 18:07:45 -0600

    App

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('¡Hola desde Express en Azure!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
